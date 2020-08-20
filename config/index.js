2687a01196731c708c5a57c8f387907e260dc2f1
jacobhsu <jacob.hsu.tw@gmail.com>
2020-08-20 10:21:45 +0800

    ShoppingCart vuex

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-webpack-vuex/',
     proxyTable: {},
 
     // Various Dev Server settings
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <ProductList/>
-
+    <ProductList />
+    <hr />
+    <ShoppingCart />
   </div>
 </template>
 
 <script>
 import ProductList from '@/components/ProductList'
+import ShoppingCart from '@/components/ShoppingCart'
 export default {
   name: 'HelloWorld',
   components: {
-    ProductList
+    ProductList,
+    ShoppingCart
   },
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
@@ -23,7 +26,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
